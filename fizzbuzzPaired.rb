7cb8d2d1819ef4210fecc08d26d48c608a45a222
Michael Poole <michaelpoole10@gmail.com>
2021-07-15 15:47:49 +0100

    The fizzbuzz rspec is finished, and the code works well

--- fizzbuzzPaired.rb
+++ fizzbuzzPaired.rb
@@ -1,12 +1,15 @@
-puts "Please enter a number"
-number = gets.chomp.to_i
 
-if number % 15 == 0
-  puts "Fizzbuzz"
-elsif number % 5 == 0
-  puts "Buzz"
-elsif number % 3 == 0
-  puts "Fizz"
-else 
-  puts number
-end
+
+def fizzbuzz(number)
+  puts "Please enter a number"
+  number = number.to_i
+  if number % 15 == 0
+    return "Fizzbuzz"
+  elsif number % 5 == 0
+    return "Buzz"
+  elsif number % 3 == 0
+    return "Fizz"
+  else 
+    return number
+  end
+end 
---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,16 @@
+require "./fizzBuzzPaired.rb"
+
+describe 'fizzbuzz' do
+    it 'returns "Fizz" when passed 3' do
+        expect(fizzbuzz(3)).to eq 'Fizz'
+    end
+    it 'returns "Buzz" when passed 5' do
+        expect(fizzbuzz(5)).to eq 'Buzz'
+    end
+    it 'returns "FizzBuzz" when passed 15' do
+        expect(fizzbuzz(15)).to eq 'Fizzbuzz'
+    end
+    it 'returns "4" when passed 4' do
+        expect(fizzbuzz(4)).to eq 4
+    end
+end
